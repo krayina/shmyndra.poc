657f5223347ac5c9dd6d29661f9a765a7b32928b
limeniye <limeniye@gmail.com>
2026-02-01 17:58:24 +0200

    feat: lazy caching for MavlinkDialectCompositor

--- Generator/PoC.Mavlink/MavlinkDialectCompositor.cs
+++ Generator/PoC.Mavlink/MavlinkDialectCompositor.cs
@@ -1,4 +1,5 @@
 ﻿using Mavlink.Dialects;
+using System.Collections.Concurrent;
 
 namespace Mavlink;
 
@@ -6,35 +7,58 @@ internal sealed class MavlinkDialectCompositor : IMavlinkDialect
 {
     private readonly IMavlinkDialect[] _dialects;
 
+    private readonly ConcurrentDictionary<uint, IMavlinkMessageInfo?> _idCache = new();
+    private readonly ConcurrentDictionary<Type, IMavlinkMessageInfo?> _typeCache = new();
+
     public MavlinkDialectCompositor(IMavlinkDialect[] dialects)
     {
-        _dialects = dialects;
+        _dialects = dialects ?? Array.Empty<IMavlinkDialect>();
     }
 
     public string Name => "DialectCompositor";
 
     public IMavlinkMessageInfo? GetInfo(Type type)
     {
-        for (int i = 0; i < _dialects.Length; i++)
+        if (_typeCache.TryGetValue(type, out var info))
         {
-            var info = _dialects[i].GetInfo(type);
-            if (info != null)
-            {
-                return info;
-            }
+            return info;
         }
-        return null;
+
+        info = FindInDialects(type);
+        _typeCache.TryAdd(type, info);
+
+        return info;
     }
 
     public IMavlinkMessageInfo? GetInfo(uint messageId)
     {
+        if (_idCache.TryGetValue(messageId, out var info))
+        {
+            return info;
+        }
+
+        info = FindInDialects(messageId);
+        _idCache.TryAdd(messageId, info);
+
+        return info;
+    }
+
+    private IMavlinkMessageInfo? FindInDialects(Type type)
+    {
+        for (int i = 0; i < _dialects.Length; i++)
+        {
+            var info = _dialects[i].GetInfo(type);
+            if (info != null) return info;
+        }
+        return null;
+    }
+
+    private IMavlinkMessageInfo? FindInDialects(uint messageId)
+    {
         for (int i = 0; i < _dialects.Length; i++)
         {
             var info = _dialects[i].GetInfo(messageId);
-            if (info != null)
-            {
-                return info;
-            }
+            if (info != null) return info;
         }
         return null;
     }
