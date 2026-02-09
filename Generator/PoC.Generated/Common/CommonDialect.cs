e6eba6876e9bc0e63cec9cad6e77e6ee0795f607
limeniye <limeniye@gmail.com>
2026-02-09 23:05:05 +0200

    chore: remove TODO comments

--- Generator/PoC.Generated/Common/CommonDialect.cs
+++ Generator/PoC.Generated/Common/CommonDialect.cs
@@ -16,18 +16,15 @@ public sealed class CommonDialect : IMavlinkDialect
 
     public string Name => "common";
 
-    // For AOT compilation. Also could works with dictionary cache.
     public IMavlinkMessageInfo? GetInfo(uint msgId) => msgId switch
     {
         0 => HeartbeatMessageInfo.Instance,
         _ => null
     };
 
-    // For AOT compilation. Also could works with dictionary cache.
     public IMavlinkMessageInfo? GetInfo(Type type)
     {
         if (type == typeof(HeartbeatMavlinkMessage)) return HeartbeatMessageInfo.Instance;
         return null;
-        // return _typeMap.TryGetValue(type, out var info) ? info : null;
     }
 }
