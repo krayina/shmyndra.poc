75c68e72031243134ee484a5ef0c5a57cfd853c3
limeniye <limeniye@gmail.com>
2026-07-15 16:14:38 +0300

    chore: remove duplicate type

--- Generator/PoC.Mavlink/MavlinkSystemState.cs
+++ Generator/PoC.Mavlink/MavlinkSystemState.cs
@@ -7,14 +7,6 @@ public enum MavlinkSystemState : byte
     Silent = 2,
 }
 
-public enum MavlinkSessionVersion : byte
-{
-    Unknown = 0,
-    V1 = 1,
-    V2 = 2,
-    Hybrid = 3,
-}
-
 public readonly struct MavlinkSystemStateChange
 {
     public MavlinkSystemStateChange(MavlinkSystemState oldState, MavlinkSystemState newState)
