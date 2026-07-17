a52e6336d5785cd41222981b217b3b0ffb6f11cc
limeniye <limeniye@gmail.com>
2026-07-17 15:27:23 +0300

    chore: update samples

--- Generator/PoC.ConsoleApp/Program.cs
+++ Generator/PoC.ConsoleApp/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Mavlink;
+using Mavlink.Common;
+using Mavlink.Transport;
+
+await using var client = await MavlinkClient.ConnectAsync("tcp://127.0.0.1:5760");
+
+client.Subscribe<HeartbeatMavlinkMessage>((msg, packet) =>
+    Console.WriteLine($"Heartbeat від sys={packet.SenderSystemId}"));
+
+
+bool canConnect = MavlinkEndpoint.TryParse("tcp://127.0.0.1:5760", out _, out var error);
+
+
+//await channel.ConnectAsync();
+
+//var drone1 = channel.GetSystem(1);
+
+//drone1.Subscribe<CargoSlotStatusMavlinkMessage>((msg, pkt) =>
+//    UpdateSlotUi(slotId: pkt.SenderComponentId, msg));
+
+//await gcs.SendToAsync(new CommandLongMavlinkMessage
+//{
+//    Command = MavCmd.DoCargoRelease /* умовно */,
+//}, drone1);
+//drone1.Subscribe<CommandAckMavlinkMessage>((ack, pkt) =>
+//    Console.WriteLine(($"Слот {pkt.SenderComponentId}: {ack.Result}"));
+
+//await gcs.SendToAsync(releaseCmd, drone1.GetComponent(26 /* слот №2 */));
+
+
+Console.WriteLine("Hello, World!");
